04fd49638a7fd5754f8d837ff22a6b9b99adeeec
Olha <olha.oleshchuk@gmail.com>
2025-08-07 16:20:11 +0300

    first try

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,21 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean acquired = semaphore.tryAcquire();
+
+        if (acquired) {
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
 }
